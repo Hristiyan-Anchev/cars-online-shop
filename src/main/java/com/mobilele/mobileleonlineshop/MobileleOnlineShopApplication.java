8b2aafe8da124ce900dc7371d49b5f217b3be0d3
hristiyan-anchev <hristian.anchev@gmail.com>
2021-01-22 10:26:06 +0000

    initial commit

---
+++ src/main/java/com/mobilele/mobileleonlineshop/MobileleOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.mobilele.mobileleonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileleOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobilele/mobileleonlineshop/MobileleOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilele.mobileleonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
